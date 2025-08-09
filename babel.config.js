202983b6bd005734a078a8ac6d07efd16ad8ef85
Rafael Freitas <rafael.vasconcelos@outlook.com>
2025-08-09 12:14:50 -0300

    Add required libs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+          '.jsx',
+          '.png',
+          '.mp4',
+          '.ios.js',
+          '.ios.jsx',
+          '.android.js',
+          '.android.jsx',
+        ],
+        alias: {
+          '@src': './src',
+        },
+      },
+    ],
+    [require.resolve('react-native-worklets/plugin')],
+  ],
 };
